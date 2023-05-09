a40cf66dc7007ad009a64cb046580b0c91a22e43
LaQuan <117905467+LaQuannT@users.noreply.github.com>
2023-05-09 13:04:47 +0100

    edited app.tsx to render static layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,17 @@ import "./App.css";
 function App() {
   return (
     <div className="app">
-      <h1>title</h1>
+      <h1>Quote Of The Day</h1>
+      <div className="quoteCard">
+        <p className="cardText">
+          "It's inspirational to see someone who is dying smile."
+        </p>
+        <p className="cardAuthor">"Arlen Specter"</p>
+      </div>
+      <div className="searchbar">
+        <input type="text" placeholder="Category of Quote" size={40} />
+        <button type="button">Search</button>
+      </div>
     </div>
   );
 }
