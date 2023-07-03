a5dc54ba129d3b17219059ddee6bd458e3eb5c82
LaQuan <117905467+LaQuannT@users.noreply.github.com>
2023-07-03 15:53:17 +0100

    build: restructure project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,20 @@
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
+    'airbnb',
+    'airbnb-typescript',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react-refresh', '@typescript-eslint', 'prettier'],
   rules: {
     'react-refresh/only-export-components': 'warn',
   },
-}
+};
