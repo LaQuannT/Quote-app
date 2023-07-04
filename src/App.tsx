881b088b43f75551757bb00129b5a90fd320ad54
LaQuan <117905467+LaQuannT@users.noreply.github.com>
2023-07-04 11:22:27 +0100

    feat: add axios fetching on click event

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
+/* eslint-disable no-console */
 import { useState } from 'react';
+import axios from 'axios';
+import QuoteCard from './components/QuoteCard';
+
+interface Data {
+  quote: string;
+  author: string;
+  category: string;
+}
 
 function App() {
   const [category, setCategory] = useState('');
+  const [data, setData] = useState<Data>();
 
-  const clickHandler = () => {
-    return null;
+  const handleClick = () => {
+    axios
+      .get(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
+        headers: {
+          'X-Api-Key': `${import.meta.env.VITE_KEY}`,
+          'Content-Type': 'application/json',
+        },
+      })
+      .then((res) => setData(res.data[0] as Data))
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          console.log(error.request);
+        } else {
+          console.log('Error', error.message);
+        }
+      });
   };
 
   return (
@@ -16,9 +44,10 @@ function App() {
         value={category}
         onChange={(e) => setCategory(e.target.value)}
       />
-      <button type="button" onClick={clickHandler}>
+      <button type="button" onClick={handleClick}>
         Search
       </button>
+      {data && <QuoteCard quote={data.quote} author={data.author} />}
     </section>
   );
 }
